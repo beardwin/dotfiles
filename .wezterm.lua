c24999870f93b8387ae4142cefa6bb24fa07d110
Ryan Baldwin <ryan.baldwin@shopify.com>
2024-12-16 16:28:38 -0800

    update pane navigation

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,10 +2,13 @@ local wezterm = require 'wezterm'
 local config = {}
 
 config.font = wezterm.font 'FiraCode Nerd Font Mono'
-config.font_size = 13
+config.font_size = 14
+
+config.adjust_window_size_when_changing_font_size = false
 
 
 config.keys = {
+  -- splitting
   {
     key = 'd',
     mods = 'CMD',
@@ -16,6 +19,8 @@ config.keys = {
     mods = 'CMD',
     action = wezterm.action.SplitVertical {domain = 'CurrentPaneDomain'},
   },
+
+  -- pane closing
   {
     key = 'w',
     mods = 'CMD',
@@ -26,11 +31,35 @@ config.keys = {
     mods = 'CMD',
     action = wezterm.action.CloseCurrentTab { confirm = true },
   },
+
+  -- clearing
   {
     key = 'k',
     mods = 'CMD',
     action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
-  }
+  },
+
+  -- pane direction
+  {
+    key = 'H',
+    mods = 'CTRL',
+    action = wezterm.action.ActivatePaneDirection 'Left',
+  },
+  {
+    key = 'L',
+    mods = 'CTRL',
+    action = wezterm.action.ActivatePaneDirection 'Right',
+  },
+  {
+    key = 'J',
+    mods = 'CTRL',
+    action = wezterm.action.ActivatePaneDirection 'Down',
+  },
+  {
+    key = 'K',
+    mods = 'CTRL',
+    action = wezterm.action.ActivatePaneDirection 'Up',
+  },
 }
 
 return config
