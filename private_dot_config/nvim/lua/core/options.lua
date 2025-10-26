4df8216bc565732fc3fa8b0850ff50e4ff734fb8
Ryan Baldwin <tech@beardwin.ca>
2025-10-26 11:13:03 -0700

    enable wordwrap for markdown files

--- private_dot_config/nvim/lua/core/options.lua
+++ private_dot_config/nvim/lua/core/options.lua
@@ -100,3 +100,11 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "markdown",
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.linebreak = true -- Optional: break at word boundaries
+	end,
+})
